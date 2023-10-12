d8c43e14a8e958b0003ff54b3e2d41ffdff68ee1
Mihir Das <mihirkantho@gmail.com>
2023-10-12 13:25:40 +0600

    adding home page

---
+++ assets/JS/custom.js
@@ -0,0 +1,134 @@
+// Nav Bar JS
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+// Home Slider
+var swiper = new Swiper(".hero_sliderr", {
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Blogs Slider
+var swiper = new Swiper(".blog", {
+  navigation: {
+    nextEl: ".blog-next",
+    prevEl: ".blog-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 5200,
+    disableOnInteraction: false,
+  },
+});
+var swiper = new Swiper(".blogTwo", {
+  navigation: {
+    nextEl: ".blog-two-next",
+    prevEl: ".blog-two-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+});
+var swiper = new Swiper(".blogThree", {
+  navigation: {
+    nextEl: ".blog-three-next",
+    prevEl: ".blog-three-prev",
+  },
+  loop: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+});
+// Student Resources Slider
+var swiper = new Swiper(".Resources", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+  breakpoints: {
+    400: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+// Countdown Timer
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0"),
+    yyyy = today.getFullYear(),
+    nextYear = yyyy + 1,
+    dayMonth = "09/30/",
+    birthday = dayMonth + yyyy;
+
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  const countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "It's my birthday!";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(x);
+      }
+    }, 0);
+})();
